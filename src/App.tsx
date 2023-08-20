45a4fe51b345a231f0922985b81e83cb22268f55
Danilo M <danilo@okn.com.br>
2023-08-20 17:35:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+// import { Component } from "./components/index";
+import "@/assets/sass/global.scss";
+
+export function App() {
+    return <></>;
+}
---
+++ src/components/Component/Component.tsx
@@ -0,0 +1,5 @@
+// import style from "./Component.module.scss";
+
+export function Component() {
+    return <></>;
+}
